b8eb9eee390710c5ebdf359a51fda505016d56e8
Igor Gonak <igor.gonak@web.de>
2023-10-24 22:48:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Bootstapped React project with following setup:
+
+- Typescript
+- Vitejs
+- Storybook
+- Mantine (hooks, core, form, dates) + PostCSS
+- Eslint
+- Prettier
+- Dayjs
+- Dependabot with pinned deps
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+const App = () => {
+  return <MantineProvider>{/* Your app here */}</MantineProvider>;
+};
+
+export default App;
